7e04b750bb0cef07856be1110b3904e5f420b33b
rteles <rteles@outlook.com>
2025-01-24 21:02:20 -0800

    inicio do curso fundamentos aspnet mvc

---
+++ DemoWebApi/DemoWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using DemoWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("alunos")]
+        public Aluno GetAluno()
+        {
+            return new Aluno { 
+            Id = 1,
+            Nome = "Rodolfo Teles",
+            Ativo = true,
+            DataNascimento = new DateTime(1986, 10, 18),
+            Email = "rteles@outlook.com"
+            };
+        }
+    }
+}
---
+++ DemoWebApi/DemoWebApi/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoWebApi.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+        public bool  Ativo { get; set; }
+
+    }
+}
